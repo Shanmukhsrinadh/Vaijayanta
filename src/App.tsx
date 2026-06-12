470b22096fcfa945f6c75d5f82a86a9ef06eb6f3
Shanmukhsrinadh <shimachalamshanmuk60297@gmail.com>
2026-06-12 15:23:59 +0530

    pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Route, Switch } from 'wouter';
+import { Router, Route, Switch } from 'wouter';
+import { useHashLocation } from 'wouter/use-hash-location';
+
 import Home from './pages/Home';
 import CityDetails from './pages/CityDetails';
 import Terms from './pages/Terms';
@@ -10,17 +12,20 @@ import WhatsAppFloatingChat from './components/WhatsAppFloatingChat';
 
 export default function App() {
   return (
-    <>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/city/:cityId" component={CityDetails} />
-        <Route path="/terms" component={Terms} />
-        <Route path="/privacy" component={Privacy} />
-        <Route path="/cookies" component={Cookies} />
-        <Route path="/credits" component={Credits} />
-        <Route component={NotFound} />
-      </Switch>
-      <WhatsAppFloatingChat />
-    </>
+    <Router hook={useHashLocation}>
+      <>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/city/:cityId" component={CityDetails} />
+          <Route path="/terms" component={Terms} />
+          <Route path="/privacy" component={Privacy} />
+          <Route path="/cookies" component={Cookies} />
+          <Route path="/credits" component={Credits} />
+          <Route component={NotFound} />
+        </Switch>
+
+        <WhatsAppFloatingChat />
+      </>
+    </Router>
   );
 }
