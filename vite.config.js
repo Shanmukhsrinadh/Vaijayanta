2fdb145bf0ec3b9f67ae6c848670cccff092db97
Shanmukhsrinadh <shimachalamshanmuk60297@gmail.com>
2026-06-12 17:27:54 +0530

    name update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Vaijayantanew/' : '/',
+  base: command === 'build' ? '/Vaijayanta/' : '/',
   plugins: [
     react(),
     tailwindcss(),
